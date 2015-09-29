b49dcb3959a59da7901c869b1dc0c1683d24416d
chris underwood & Jason Lane <student@epicodus.com>
2015-09-28 17:27:06 -0700

    Add delete to listing

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('listing', params.listing_id)
+  },
+  actions: {
+    deleteListing(listing) {
+      if(confirm("Are you sure?")) {
+        this.transitionTo('subCategory', listing.get('subCategory'));
+        listing.destroyRecord();
+
+      }
+    }
   }
+
 });
